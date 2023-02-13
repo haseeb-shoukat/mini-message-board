9ff2276ac06344ab3b57964c247f82695742ca2c
haseeb-shoukat <haseebshoukat52@gmail.com>
2023-02-13 19:34:39 +0300

    Add messages array and pass it to view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
+const messages = [
+  { text: "Hello, how are you doing", user: "Dylan", added: new Date() },
+  { text: "I am doing fine, thank-you", user: "Grigory", added: new Date() },
+  { text: "I am ill.", user: "Dunechka", added: new Date() },
+];
+
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Message Board", messages: messages });
 });
 
 module.exports = router;
